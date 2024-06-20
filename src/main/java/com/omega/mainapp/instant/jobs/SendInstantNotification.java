dc6d14adbd2f7a38b8afeb31412fd1f1a83d7cbe
ManojKannanD <fabulousmanoj27@gmail.com>
2024-06-21 00:09:54 +0530

    adding base conf files

---
+++ src/main/java/com/omega/mainapp/instant/jobs/SendInstantNotification.java
@@ -0,0 +1,11 @@
+package com.omega.mainapp.instant.jobs;
+
+import com.omega.jobservice.instantjob.InstantJob;
+import org.apache.commons.chain.Context;
+
+public class SendInstantNotification extends InstantJob {
+    @Override
+    public void execute(Context context) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/omega/mainapp/scheduled/jobs/SendScheduledNotifications.java
@@ -0,0 +1,11 @@
+package com.omega.mainapp.scheduled.jobs;
+
+import com.omega.jobservice.jobconfig.JobContext;
+import com.omega.jobservice.scheduledjob.ScheduledJob;
+
+public class SendScheduledNotifications extends ScheduledJob {
+    @Override
+    public void execute(JobContext jobContext) throws Exception {
+
+    }
+}
