9f71682b2f6c0f09debdf0a3d26074ff56e0177b
ManojKannanD <fabulousmanoj27@gmail.com>
2024-06-28 12:32:25 +0530

    moving project to Spring Structure

--- src/main/java/com/omega/mainapp/scheduled/jobs/SendScheduledNotifications.java
+++ src/main/java/com/omega/mainapp/scheduled/jobs/SendScheduledNotifications.java
@@ -2,17 +2,14 @@ package com.omega.mainapp.scheduled.jobs;
 
 import com.omega.jobservice.jobconfig.JobContext;
 import com.omega.jobservice.scheduledjob.ScheduledJob;
-import org.springframework.beans.factory.annotation.Autowired;
 import com.omega.mainapp.sample.service.StudentService;
 import com.omega.mainapp.sample.model.Student;
 import com.omega.mainapp.utils.TimeUtils;
 
 public class SendScheduledNotifications extends ScheduledJob {
-    @Autowired
-    StudentService studentService;
-
     @Override
     public void execute(JobContext jobContext) throws Exception {
+        StudentService studentService = getApplicationContext().getBean(StudentService.class);
         Student student = new Student(jobContext.getJobId(), "Student", TimeUtils.getCurrentTime());
         studentService.addStudent(student);
     }
